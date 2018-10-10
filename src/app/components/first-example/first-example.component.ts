2ea1d466c51c455b3d16a4ae9f1f907914306608
Hitzu <roberto@enigmo.mx>
2018-10-10 12:19:04 -0500

    [Add] (first-example) first example with trigger, change on click

--- src/app/components/first-example/first-example.component.ts
+++ src/app/components/first-example/first-example.component.ts
@@ -1,12 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style } from '@angular/animations'
 
 @Component({
   selector: 'app-first-example',
   templateUrl: './first-example.component.html',
-  styleUrls: ['./first-example.component.css']
+  styleUrls: ['./first-example.component.css'],
+  animations : [
+    trigger('clickedState', [
+      state('default', style({
+        backgroundColor: 'orange',
+        width : '100px',
+        height : '100px'
+      })),
+      state('clicked', style({
+        backgroundColor: 'blue',
+        width : '300px',
+        height : '50px'
+      }))
+    ])
+  ]
 })
 export class FirstExampleComponent implements OnInit {
-
+  public clickInfo: string = "default"
   constructor() { }
 
   ngOnInit() {
