df7c98ce4565dd67638460a245a1f71c0efaa39c
Hitzu <roberto@enigmo.mx>
2018-10-10 14:32:30 -0500

    [Add] (first example) OnMouseDown

--- src/app/components/first-example/first-example.component.ts
+++ src/app/components/first-example/first-example.component.ts
@@ -17,13 +17,22 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
         width : '300px',
         height : '50px'
       })),
+      state('mousedown', style ({
+        backgroundColor: 'red',
+        border : '1px solid black ',
+        width : '100px',
+        height : '100px'
+      })),
       transition('default => clicked', animate('200ms 500ms ease-in')),
-      transition('clicked => default', animate('200ms 500ms ease-in'))
+      transition('clicked => default', animate('200ms 500ms ease-in')),
+      transition('default => mousedown', animate('200ms ease-in')),
+      transition('mousedown => default', animate('200ms ease-in'))
     ])
   ]
 })
 export class FirstExampleComponent implements OnInit {
   public clickInfo: string = "default"
+  public mouseDownInfo : string = 'default'
   constructor() { }
 
   ngOnInit() {
@@ -34,6 +43,15 @@ export class FirstExampleComponent implements OnInit {
     setTimeout( () => {
       this.clickInfo = 'default'
     }, 2000)
+  }
+
+  OnMouseDownSimple(){
+    console.log("entro")
+    this.mouseDownInfo = "mousedown";
+  }
+
+  OnMouseUpSimple(){
+    this.mouseDownInfo = 'default';
   }
 
 }
